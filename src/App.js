b554775df159f366e0aaff5e4241e41646de07d2
thipereira02 <thiago.spereira02@gmail.com>
2022-02-08 19:39:20 -0300

    Setting project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import GlobalStyle from './layouts/GlobalStyle';
+import Home from './pages/Home';
+
+export default function App() {
+    return (
+        <BrowserRouter>
+            <GlobalStyle />
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
